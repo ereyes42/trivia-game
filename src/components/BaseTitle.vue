d9d8a68cdbf2b1cd29ba50cac74b01aa10e5beb9
Ember Reyes <ereyes42@stu.southtexascollege.edu>
2022-10-12 12:59:31 -0500

    added base title

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="brand">
+    <slot name="logo" />
+    <h1 class="title">
+      <slot />
+    </h1>
+    <slot name="logo" />
+  </div>
+</template>
+<style lang="postcss" scoped>
+.brand {
+  @apply flex items-center justify-center gap-4;
+
+  & :slotted(img) {
+    @apply h-12 w-12;
+  }
+  & .title {
+    @apply text-4xl font-semibold uppercase tracking-wide text-teal-600;
+  }
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import useAPI from '@/composables/useApi'
+import BaseTitle from '../components/BaseTitle.vue'
 
 const api = useAPI()
 const categories = ref([])
@@ -11,11 +12,12 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="brand">
-    <img class="logo" src="logo.svg" alt="logo" />
-    <h1 class="title">Trivia Game</h1>
-    <img class="logo" src="logo.svg" alt="logo" />
-  </div>
+  <BaseTitle>
+    <template #logo>
+      <img src="logo.svg" alt="logo" />
+    </template>
+    Triviantastic
+  </BaseTitle>
   <div class="categories">
     <RouterLink
       v-for="category in categories"
@@ -28,17 +30,6 @@ onMounted(async () => {
 </template>
 
 <style lang="postcss" scoped>
-.brand {
-  @apply flex items-center justify-center gap-4;
-
-  & .logo {
-    @apply h-14 w-14;
-  }
-  & .title {
-    @apply text-4xl font-semibold uppercase tracking-wide text-teal-600;
-  }
-}
-
 .categories {
   @apply grid flex-grow grid-cols-4 gap-10;
   & .category {
